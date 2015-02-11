f74be36701653af6dbd23b22aa6ecb509eb45f6b
Vinh <stevenlu7@yahoo.com>
2015-02-10 17:44:43 -0800

    Added arraylist with hashmap for multi column view function

--- gen/com/example/inventory/R.java
+++ gen/com/example/inventory/R.java
@@ -25,19 +25,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080011;
         public static final int addButton=0x7f080005;
         public static final int addComplete=0x7f080004;
-        public static final int backButton=0x7f08000d;
+        public static final int backButton=0x7f08000e;
         public static final int deleteButton=0x7f080006;
-        public static final int deleteInPopup=0x7f08000c;
-        public static final int deleteItemName=0x7f08000b;
+        public static final int deleteInPopup=0x7f08000d;
+        public static final int deleteItemName=0x7f08000c;
+        public static final int header=0x7f080009;
+        public static final int headerQuantity=0x7f08000a;
         public static final int newItemAmount=0x7f080003;
         public static final int newItemName=0x7f080001;
-        public static final int popup_element=0x7f08000a;
-        public static final int res=0x7f080009;
+        public static final int popup_element=0x7f08000b;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
+        public static final int textViewName=0x7f08000f;
+        public static final int textViewQuantity=0x7f080010;
         public static final int viewButton=0x7f080007;
         public static final int viewTest=0x7f080008;
     }
@@ -46,6 +49,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int activity_view=0x7f030002;
         public static final int delete_popup=0x7f030003;
+        public static final int view_item=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/inventory/ViewItemActivity.java
+++ src/com/example/inventory/ViewItemActivity.java
@@ -1,65 +1,107 @@
 package com.example.inventory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.ListActivity;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.view.Menu;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-public class ViewItemActivity extends FragmentActivity implements
+public class ViewItemActivity extends ListActivity implements
 		LoaderManager.LoaderCallbacks<Cursor> {
 	TextView resultView = null;
 	CursorLoader cursorLoader;
-	
+
 	private static final String AUTHORITY = "com.example.inventory.MyContentProvider";
 	private static final String TABLE_ITEMS = "items";
-	private static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
-			+ "/" + TABLE_ITEMS);
-	private static final String COLUMN_ITEMS_ID = "_id";
-	private static final String COLUMN_ITEMS_NAME = "name";
-	private static final String COLUMN_ITEMS_QUANTITY = "quantity";
+
+	//ArrayList<String> list = new ArrayList<String>();
+	//ArrayAdapter<String> adapter;
+	TextView nameHeader;
+	TextView quantityHeader;
+	ListView listView;
+
+	ArrayList<HashMap<String, String>> allItemsList;
+	HashMap<String, String> map;
+	SimpleAdapter simpleAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view);
-		resultView = (TextView) findViewById(R.id.res);
+
+		//adapter = new ArrayAdapter<String>(this,
+			//	android.R.layout.simple_list_item_1, list);
+
+		nameHeader = (TextView) findViewById(R.id.header);
+		quantityHeader = (TextView) findViewById(R.id.headerQuantity);
+		quantityHeader.setText("QUANTITY");
+		quantityHeader.setTypeface(null, Typeface.BOLD);
+		quantityHeader.setTextColor(Color.BLACK);
+		
+		nameHeader.setText("NAME");
+		nameHeader.setTypeface(null, Typeface.BOLD);
+		nameHeader.setTextColor(Color.BLACK);
+		// textView.setWidth(10);
+		// listView = getListView();
+		// listView.addHeaderView(textView);
+		listView = (ListView) findViewById(android.R.id.list);
+		allItemsList = new ArrayList<HashMap<String, String>>();
+		map = new HashMap<String, String>();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		getSupportLoaderManager().initLoader(1, null, this);
+		getLoaderManager().initLoader(0, null, this);
 		return true;
 	}
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-		cursorLoader = new CursorLoader(
-				this,
-				Uri.parse("content://" + AUTHORITY
-						+ "/" + TABLE_ITEMS),
-				null, null, null, null);
+		cursorLoader = new CursorLoader(this, Uri.parse("content://"
+				+ AUTHORITY + "/" + TABLE_ITEMS), null, null, null, null);
 		return cursorLoader;
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
 		cursor.moveToFirst();
-		StringBuilder res = new StringBuilder();
+		// StringBuilder res = new StringBuilder();
 		while (!cursor.isAfterLast()) {
-			res.append("\n" + cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_ID))
-					+ "-" + cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_NAME))
-					+ ":" + cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_QUANTITY)));
+			// res.append("\n" +
+			// cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_ID))
+			// + "-" +
+			// cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_NAME))
+			// + ":" +
+			// cursor.getString(cursor.getColumnIndex(COLUMN_ITEMS_QUANTITY)));
+			// list.add(cursor.getString(1) + "\t" + "\t" + "\t" + "\t" + "\t" +
+			// cursor.getString(2));
+			map = new HashMap<String, String>();
+			map.put("name", cursor.getString(1));
+			map.put("quantity", cursor.getString(2));
+			allItemsList.add(map);
+			//adapter.notifyDataSetChanged();
 			cursor.moveToNext();
 		}
-		resultView.setText(res);
+		// resultView.setText(res);
+		// setListAdapter(adapter);
+		simpleAdapter = new SimpleAdapter(this, allItemsList, R.layout.view_item,
+				new String[] { "name", "quantity" }, new int[] {
+						R.id.textViewName, R.id.textViewQuantity });
+		listView.setAdapter(simpleAdapter);
 	}
 
 	@Override
